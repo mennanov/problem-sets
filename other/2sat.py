c3821db2629562e9f1a26614b09e5373abdb3ffa
Renat Mennanov <renat@mennanov.ru>
2014-08-13 12:16:37 +0400

    2sat test cases

--- other/2sat.py
+++ other/2sat.py
@@ -20,49 +20,67 @@ class Papadimitriou(object):
         """
         For every variable create a list of clauses where this variable occurs.
         """
-        matches = defaultdict(lambda: [])
+        matches = defaultdict(lambda: set())
         for c in clauses:
-            matches[abs(c[0])].append(c)
-            matches[abs(c[1])].append(c)
+            matches[abs(c[0])].add(c)
+            matches[abs(c[1])].add(c)
         return matches
 
     def run(self):
         n = len(self.vars)
-        for _ in xrange(int(math.log(n, 2))):
+        for _ in xrange(2 * int(math.log(n, 2))):
             # choose random initial assignment
             for i in xrange(1, n):
                 self.vars[i] = random.choice([True, False])
 
-            for j in xrange(2 * n ^ 2):
-                # perform local search
-                failed = []
-                for c in self.clauses:
-                    if not self._solve(c):
-                        failed.append(c)
+            # evaluate each clause
+            failed = self._solve(self.clauses)
+            # print 'start local search', self.vars
+            counter = 0
+            while failed and counter < 2 * n ^ 2:
+                # print 'failed: ', failed, len(failed)
+                # flip random variable in a random failed clause and repeat local search on the corresponding clauses
+                clause = random.sample(failed, 1)[0]
+                proceed = True
+                for var in clause:
+                    if proceed:
+                        var = abs(var)
+                        # print 'flip', var, 'to', not self.vars[var]
+                        self.vars[var] = not self.vars[var]
+                        # iterate over involved clauses
+                        failed_involved = self._solve(self.matches[var])
 
-                if not failed:
-                    # the problem is solvable
-                    return True
-                else:
-                    # flip random variable in a random failed clause and repeat local search
-                    random_clause = random.choice(failed)
-                    var = abs(random_clause[random.choice([0, 1])])
-                    self.vars[var] = not self.vars[var]
+                        if failed_involved:
+                            # add new failed clauses
+                            # print 'flip not helped', var, failed_involved, len(failed_involved)
+                            failed |= failed_involved
+                        else:
+                            # conflict with this particular variable is resolved
+                            # print 'resolved conflicts for', var
+                            failed -= self.matches[var]
+                            proceed = False
+                counter += 1
+            if not failed:
+                return True
         return False
 
-    def _solve(self, clause):
-        v1 = self.vars[clause[0]] if clause[0] > 0 else not self.vars[abs(clause[0])]
-        v2 = self.vars[clause[1]] if clause[1] > 0 else not self.vars[abs(clause[1])]
-        return v1 or v2
+    def _solve(self, clauses):
+        failed = set()
+        for clause in clauses:
+            v1 = self.vars[clause[0]] if clause[0] > 0 else not self.vars[abs(clause[0])]
+            v2 = self.vars[clause[1]] if clause[1] > 0 else not self.vars[abs(clause[1])]
+            if not(v1 or v2):
+                failed.add(clause)
+        return failed
 
 if __name__ == '__main__':
-    with open('test.txt') as fp:
+    import sys
+    with open(sys.argv[1]) as fp:
         n = int(fp.readline())
         clauses = []
-        vars = [False] * (n + 1)
         for i, line in enumerate(fp):
             data = line.split()
             clauses.append((int(data[0]), int(data[1])))
 
-        p = Papadimitriou(clauses, vars)
+        p = Papadimitriou(clauses, [None] * (n + 1))
         print p.run()
